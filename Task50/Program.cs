c264184e531d72cb3f5a45a43395d52a73cecc5a
Dmitrij <sdma7@mail.ru>
2022-09-26 13:49:27 +0300

    решение задачи 50 2 варианта

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -6,44 +6,84 @@
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
 
-Console.Clear();
-int[,] CreateMatrixRndInt(int rows, int columns)
+
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца ");
+int[,] numbers = new int[6, 8];
+CreateMatrixRndInt(numbers);
+PrintMatrix(numbers);
+
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+void CreateMatrixRndInt(int[,] matrix)
 {
-    int[,] matrix = new int[rows, columns];
-    Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(1, 11);
+            matrix[i, j] = new Random().Next(1, 10);
         }
     }
-    return matrix;
 }
 void PrintMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],2}, ");
-            else Console.Write($"{matrix[i, j],2} ");
+            Console.Write(matrix[i, j] + " ");
         }
-        Console.WriteLine("]");
+        Console.WriteLine();
     }
+    Console.WriteLine();
 }
-void SearchNumm(int[,] array, int pos)
+
+int ReadInt(string message)
 {
-    int row = pos / 10;
-    int column = pos % 10;
-    if ((row > 3) || (column > 4)) Console.WriteLine($"{pos} -> такого числа нет в массиве");
-    else Console.WriteLine($"Значение элемента равно {array[row - 1, column - 1]}"); 
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
 }
 
-int[,] array2D = CreateMatrixRndInt(3, 4);
-PrintMatrix(array2D);
-Console.Write($"Введите позицию элемента: ");
-int position = Convert.ToInt32(Console.ReadLine());
-SearchNumm(array2D, position);
 
+
+
+// Console.Clear();
+// int[,] CreateMatrixRndInt(int rows, int columns)
+// {
+//     int[,] matrix = new int[rows, columns];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(1, 11);
+//         }
+//     }
+//     return matrix;
+// }
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("[");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],2}, ");
+//             else Console.Write($"{matrix[i, j],2} ");
+//         }
+//         Console.WriteLine("]");
+//     }
+// }
+// void SearchNumm(int[,] array, int pos)
+// {
+//     int row = pos / 10;
+//     int column = pos % 10;
+//     if ((row > 3) || (column > 4)) Console.WriteLine($"{pos} -> такого числа нет в массиве");
+//     else Console.WriteLine($"Значение элемента равно {array[row - 1, column - 1]}"); 
+// }
+
+// int[,] array2D = CreateMatrixRndInt(3, 4);
+// PrintMatrix(array2D);
+// Console.Write($"Введите позицию элемента: ");
+// int position = Convert.ToInt32(Console.ReadLine());
+// SearchNumm(array2D, position);
